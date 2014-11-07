6e4c1fb11ca8edb0f909553c003694f54027cd58
Ryan Moore <moorer@udel.edu>
2014-11-07 12:31:28 -0500

    Add parse-length-summary-for-r.rb

--- scripts/parse-length-summary-for-r.rb
+++ scripts/parse-length-summary-for-r.rb
@@ -2,17 +2,31 @@
 
 Signal.trap("PIPE", "EXIT")
 
+# 'EColik12CompleteGenome.length_cutoff_summary.no-errors.no-zeros.txt'
+fname = ARGV[0]
+
 last_shortname = ''
 last_cutoff = ''
-File.open('EColik12CompleteGenome.length_cutoff_summary.no-errors.no-zeros.txt', 'r').each_line do |line|
+conf_levels = []
+File.open(fname, 'r').each_line do |line|
   if line.start_with?('basename')
-    puts line
+    puts line.chomp.split(' ').insert(5, ['new.short.name', 'confidence.level']).flatten.join(' ')
   else
     name, len, cov, kmer, shortname, num, cutoff, low, conf, high = line.chomp.split(' ')
 
-    # sometimes the 0 length cutoff calculation line is doubled
-    unless last_shortname == shortname && last_cutoff == 0 && cutoff == 0
-      # add the confidence cutoff as a column
+    # add the confidence cutoff as a column
 
-      # there is a weird thing where the confidence might not roudn to the proper cutoff, eg, in the file int might have 0.97, but the actual cutoff would be 0.95
-      (confidence.to_f * 100).round
+    # because of the way these cufoffs are set up gotta do this weird thing here....
+    if last_shortname != shortname # ie starting a new set
+      # make a list of the confidence levels for this particular set
+      # gotta do this each time cos the lower bound could change
+      conf_levels = ((conf[2] + '0').to_i .. 95).step(5).to_a
+    end
+
+    this_conf_level = conf_levels.shift
+    new_short_name = [name, len, cov, kmer, this_conf_level].join('.')
+    puts line.chomp.split(' ').insert(5, [new_short_name, this_conf_level]).flatten.join(' ')
+    
+    last_shortname = shortname
+  end
+end
