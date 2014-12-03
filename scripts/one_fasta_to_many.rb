5c446eb618317000bae078f0955a25ec3771a2f3
Ryan Moore <moorer@udel.edu>
2014-12-03 15:12:28 -0500

    Add scripts

---
+++ scripts/one_fasta_to_many.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'parse_fasta'
+
+Signal.trap("PIPE", "EXIT")
+
+outdir = ARGV[1]
+
+FastaFile.open(ARGV.first, 'r').each_record do |header, sequence|
+  f = File.open(File.join(outdir, "#{header.downcase.gsub(/[ |,.\/]+/, '_')}.applepie.fa"), 'w')
+  f.puts ">#{header}\n#{sequence}"
+  f.close
+end
---
+++ scripts/shell/small_kmer_sweep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+fasta=$1
+outdir=/home/moorer/silly-jawn/output
+
+length=250
+coverage=25
+
+working_dir=/home/moorer/projects/length_cutoff
+
+do for kmer in {41,81,141,201};
+do time ruby $working_dir/scripts/pipeline-ray.rb -f \
+    $fasta \
+    -o $outdir \
+    -b $working_dir/scripts/ \
+    -t 20 -k $kmer --no-kmer-sweep \
+    1> $outdir/pipeline-ray.$length.$coverage.$kmer.out.txt \
+    2> $outdir/pipeline-ray.$length.$coverage.$kmer.err.txt;
+done;
+
